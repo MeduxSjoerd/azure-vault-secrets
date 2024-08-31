1f99c31571bfcc642e0b4fe0b83c463fa880ec72
MeduxSjoerd Medux <sjoerd.marsman@medux.nl>
2024-08-31 11:03:35 +0000

    aanpassen van de secret

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.VisualBasic;
 
 namespace dotnetcore
 {
@@ -6,7 +9,37 @@ namespace dotnetcore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var secretName = "MySecret";
+            var keyVaultName = "my-vault-no-protect";
+            var kvUri = "https://my-vault-no-protect.vault.azure.net";
+
+            var options = new SecretClientOptions(){
+                Retry = {
+                    Delay = TimeSpan.FromSeconds(2),
+                    MaxDelay = TimeSpan.FromSeconds(16),
+                    MaxRetries = 5,
+                    Mode = Azure.Core.RetryMode.Exponential
+                }
+            };
+
+            var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential(), options);
+            KeyVaultSecret secret = client.GetSecret(secretName);
+
+
+            Console.WriteLine($"GetSecret: {secret.Value}");
+            Console.Write("Enter Secret > ");
+
+            var secretValue = Console.ReadLine();
+            client.SetSecret(secretName, secretValue);
+
+            Console.Write("SetSecret:");
+            Console.Write($" Key: + {secretName}");
+            Console.Write($" Value: + {secretValue}");
+            Console.WriteLine($"GetSecret: {secret.Value}");
+
+            client.StartDeleteSecret(secretName);
+            Console.WriteLine($"StartDeleteSecret: {keyVaultName}");
+            Console.WriteLine($"GetSecret: {secret.Value}");
         }
     }
 }
